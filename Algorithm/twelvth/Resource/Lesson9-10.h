ca5c357abe03102a44d6a90e81f7b45941523d55
Eduard <49712299+EdKrupenin@users.noreply.github.com>
2021-06-06 02:48:54 +0300

    12

---
+++ Algorithm/twelvth/Resource/Lesson9-10.h
@@ -0,0 +1,202 @@
+#pragma once
+#define T int
+#define true 1 == 1
+#define false 1 != 1
+typedef int boolean;
+
+#pragma region Односвязные списки
+/// <summary>
+/// Структура узла односвязного списка
+/// </summary>
+typedef struct Knot {
+	int date;
+	char name;
+	struct Knot* next;
+}Knot;
+
+/// <summary>
+/// Структура односвязного списка
+/// </summary>
+typedef struct List {
+	Knot* head;
+	int size;
+}List;
+
+/// <summary>
+/// Инициализация нового списка
+/// </summary>
+/// <param name="lst">Указатель на список</param>
+void init(List* lst);
+
+/// <summary>
+/// Добавление нового элемента
+/// </summary>
+/// <param name="lst">Указатель на список</param>
+/// <param name="data">Новый элемент</param>
+void insertList(List* lst, int data, char name);
+
+/// <summary>
+/// Удаление элементов из связного списка
+/// </summary>
+/// <param name="lst">ссылка на головной элемент и кол-во</param>
+/// <param name="data">данные которые надо удалить</param>
+/// <returns>данные которые надо удалить</returns>
+Knot* rm(List* lst, int data);
+
+/// <summary>
+/// Функция копирования односвязного листа
+/// </summary>
+/// <param name="Lst">Указатель на список который надо скопировать</param>
+/// <returns>Скопированный список</returns>
+List* copyList(List* lst);
+
+/// <summary>
+/// Функция проверки отсортирован ли односвязный список
+/// </summary>
+/// <param name="lst">Указатель на список</param>
+/// <returns>Результат проверки</returns>
+bool sortList(List* lst);
+
+void printKnot(Knot* k);
+
+void printList(List* lst);
+#pragma endregion
+
+#pragma region Двусвязные списки
+/// <summary>
+/// Структура узла двусвязного списка 
+/// </summary>
+typedef struct Node {
+	char data;
+	struct Node* next;
+	struct Node* previously;
+}Node;
+
+/// <summary>
+/// Структура двусвязного списка
+/// </summary>
+typedef struct DuoList {
+	Node* head;
+	Node* tail;
+	int size;
+}DuoList;
+
+void init(DuoList* lst);
+void pushFront(DuoList* lst, char data);
+void pushBack(DuoList* lst, char data);
+Node* rm(DuoList* lst, int data);
+Node* remFront(DuoList* lst);
+Node* remBack(DuoList* lst);
+void printNode(Node* k);
+void printDuoList(DuoList* lst);
+#pragma endregion
+
+#pragma region Стэк
+/// <summary>
+/// Структура узла односвязного списка
+/// </summary>
+typedef struct NodeStack {
+	T data;
+	struct NodeStack* next;
+}NodeStack;
+
+/// <summary>
+/// Структура односвязного списка
+/// </summary>
+typedef struct ListStack {
+	NodeStack* head;
+	int size;
+}ListStack;
+
+void init(ListStack* lst);
+
+boolean push(ListStack* stack, T value);
+
+T pop(ListStack* stack);
+
+#pragma endregion
+
+#pragma region Очередь на основе двусвязного списка
+/// <summary>
+/// Структура узла очереди
+/// </summary>
+typedef struct NodeQueue {
+	T data;
+	struct NodeQueue* next;
+	struct NodeQueue* previously;
+}NodeQueue;
+
+/// <summary>
+/// Структура очереди
+/// </summary>
+typedef struct Queue {
+	NodeQueue* head;
+	NodeQueue* tail;
+	int size;
+}QueueList;
+
+/// <summary>
+/// Инициализация очереди
+/// </summary>
+/// <param name="queue"></param>
+void init(QueueList* queue);
+
+/// <summary>
+/// Добавление элементов в очередь
+/// </summary>
+/// <param name="lst">Ссылка на список</param>
+/// <param name="data">Данные для добавления</param>
+boolean enqueue(QueueList* lst, T data);
+
+/// <summary>
+/// Удаление элемента элементов из очереди
+/// </summary>
+/// <param name="lst">ссылка на очереди</param>
+/// <returns>Результат удаления</returns>
+T dequeue(QueueList* lst);
+#pragma endregion
+
+#pragma region Динамические массивы
+
+/// <summary>
+/// Функция выделяет память под двумерный массив 
+/// </summary>
+/// <param name="row">размер строк</param>
+/// <param name="col">размер столбцов</param>
+/// <returns>возвращаем ссылку на фрагмент выдленной памяти</returns>
+int** init2Array(int col, int row);
+
+/// <summary>
+///  Функция для очистки памяти
+/// </summary>
+/// <param name="arr">массив который надо удалить</param>
+void clear2Araay(int** arr);
+
+/// <summary>
+/// Функция - заполнение массива конкретными значениями
+/// </summary>
+/// <param name="matrix"> ссылка на массив</param>
+/// <param name="cols">количество колонок</param>
+/// <param name="row">количество строк</param>
+/// <param name="leghtAgrs">длина аргументов массива</param>
+/// <param name="">Аргументы массива</param>
+void setLineValues(int** matrix, int cols, int row, int leghtAgrs, ...);
+
+void print2dArray(int** array, int row, int col);
+
+void print1dArray(int* arr, int row);
+
+void quickSort(int* data, int const len);
+
+int get2dInt(int** matrix, int row, int col);
+
+void set2dInt(int** matrix, int row, int col, int num);
+
+void init1dArrayNull(int* arr, int size);
+
+void init2dArrayNum(int** arr, int row, int col, int num);
+
+void print2dmatrix(int** array, int row, int col);
+
+void insertionSort(int data[], int lenD);
+#pragma endregion
---
+++ Algorithm/twelvth/lesson 12-13.cpp
@@ -0,0 +1,298 @@
+﻿#include <iostream>
+#include <fstream>
+#include "Resource/Lesson9-10.h"
+
+#define true 1 == 1
+#define false 1 != 1
+typedef int boolean;
+const int fiftytree = 50;
+const int countKnot = 10;
+std::ifstream file;
+
+const int n = 6; //Количество вершин графа...
+//Матрица смежности
+int matrix[n][n] = {
+	//   a,b,c,d,e,f
+		{0,1,1,0,0,0}, //a
+		{0,0,0,1,1,1}, //b
+		{0,0,0,0,0,1}, //c
+		{1,0,0,0,0,0}, //d
+		{0,0,0,0,0,0}, //e
+		{0,0,0,0,1,0}, //f
+};
+int visited[n] = { 0 }; //Массив посещенных вершин
+int refernce[n] = { 0 };
+
+#pragma region Обход в глубину
+/// <summary>
+/// переходы на глубину
+/// </summary>
+/// <param name="st"></param>
+void depthTravers(int st) {
+	int guidingStar;
+	std::cout << "You were here - " << char(0x61 + st) << " ";
+	visited[st] = 1;
+	
+	//Проверяем связи "ребра" с другими вершинами
+	for (guidingStar = 0; guidingStar < n; guidingStar++) {
+		if (matrix[st][guidingStar] && visited[guidingStar] != 1) {
+			depthTravers(guidingStar);
+		}
+			
+	}
+}
+
+/// <summary>
+/// Проход в глубину c использованием Stack
+/// </summary>
+/// <param name="matrix">Матрица смежности</param>
+/// <param name="start">Точка старта</param>
+/// <param name="size">Количество вершин графа (размер матрицы)</param>
+void depthTraversStack(int** matrix, int start, const int size) {
+	//Создаем и инициализируем стэк
+	ListStack* stackTravers = new ListStack;
+	init(stackTravers);
+	//Создаем и обнуляем массив посещений
+	int* visiteD = new int[size];
+	init1dArrayNull(visiteD, size);
+	//Записываем в очередь стартовую точку
+	push(stackTravers, start);
+	while (stackTravers->size > 0) {
+		int index = pop(stackTravers); //забираем узел с вершины стэка
+		if (visiteD[index] == 1) continue; //если там были то берем следующий
+		visiteD[index] = 1; // отмечаемся что мы тут были
+		std::cout << "You were here - " << char(0x61 + index) << " ";
+		//Смотрим куда можно пойти из вершины index по строке матрицы смежности...
+		for (size_t i = 0; i < size; i++)
+		{
+			//Забираем значение массива в строке index в колонке i
+			if (get2dInt(matrix, index, i) == 1 && visiteD[i] == 0)
+				//Если мы там еще не были пушим в стэк
+				push(stackTravers, i);
+		}
+	}
+	//Если рекурсия идет в прямом порядке то стэк - в обратном в данном случае начинаем с С
+	//Закончиться когда все вершины будут посещены
+	delete[] visiteD;
+}
+#pragma endregion
+
+#pragma region Обход в ширину
+/// <summary>
+/// Обход в ширину на основе очереди и цикла while
+/// </summary>
+/// <param name="matrix">Матрица смежности</param>
+/// <param name="start">Точка старта</param>
+/// <param name="size">Количество вершин графа (размер матрицы)</param>
+void widthTravers(int** matrix, int start, const int size) {
+	//Создаем и инициализируем очередь
+	QueueList* queueTravers = new QueueList;
+	init(queueTravers);
+	//Создаем и обнуляем массив посещений
+	int* visiteD = new int[size];
+	init1dArrayNull(visiteD, size);
+	//Записываем в очередь стартовую точку
+	enqueue(queueTravers, start);
+	while (queueTravers->size > 0) {
+		int index = dequeue(queueTravers);
+		if (visiteD[index] == 1) continue;
+		visiteD[index] = 1;
+		std::cout << "You were here - " << char(0x61 + index) << " ";
+		for (size_t i = 0; i < size; i++)
+		{
+			//Забираем значение массива в строке index в колонке i
+			if (get2dInt(matrix, index, i) == 1 && visiteD[i] == 0){
+				//Если там 1 (значит есть связь и мы там не были то welcome
+				enqueue(queueTravers, i);
+			}
+			//считаем количество ссылок на смежные вершины
+			if (get2dInt(matrix, index, i) == 1) refernce[index] += 1;
+		}
+	}
+	//Закончиться когда все вершины будут посещены
+	delete[] visiteD;
+}
+#pragma endregion
+
+#pragma region Поиск кратчайшего пути в графе
+const int WIDTH = 10;
+const int HEIGTH = 8;
+const int OBSTACLE = -1; //препятствие
+const int BLANK = -2;    //пустая ячейка
+
+//Хранение координат точек пути
+int pointX[HEIGTH * WIDTH]; // Данный размер обусловлен тем,
+int pointY[HEIGTH * WIDTH]; //что никогда не известно какой длины будет путь
+
+int len; // длина пути
+
+/// <summary>
+/// Функция поиска пути 
+/// </summary>
+/// <param name="grid">Поле</param>
+/// <param name="sx">Координата X стартовой точки</param>
+/// <param name="sy">Координата Y стартовой точки</param>
+/// <param name="ex">Координата X финишной точки</param>
+/// <param name="ey">Координата Y финишной точки</param>
+/// <returns></returns>
+int lee(int** grid, int sx, int sy, int ex, int ey) {
+	// Данные массивы дают возможность в 4х направлениях просматривать смежные ячейки
+	int dx[4] = { 1,0,-1,0 }; //Они хранят смещение относительно текущей точки по осям ХУ
+	int dy[4] = { 0,1,0,-1 };
+	//Взяв из каждого массива i-й элемент получим направление по которому будем смотреть на ячейку
+	/*
+			0,-1
+			 |
+	 -1,0 - 0,0 - 1,0
+			 |
+			0,1
+	*/
+	//Счетчики циклов при проходе по рабочему полю
+	int x = 0;
+	int y = 0;
+	int vector; //Возможные варианты смещения относительно текущей точки
+	int distance = 0; //Номер шага волны(то что пишем в ячейку)
+	int stop; //Признак невозможности прохода(когда кончились узлы)
+
+	if (grid[sy][sx] == OBSTACLE || grid[ey][ex] == OBSTACLE)
+		return 0; //Если одна из точек лежит на препятствии
+	grid[sy][sx] = 0; //Начало (этап распространения волны)
+	do {
+		stop = true;
+		for (y = 0; y < HEIGTH; ++y) {
+			for (x = 0; x < WIDTH; ++x) {
+				//Если ху = шагу волны накатываем новый
+				if (grid[y][x] == distance) {
+					for (vector = 0; vector < 4; vector++) {
+						int nextX = x + dx[vector];
+						int nextY = y + dy[vector];
+						if (nextX >= 0 && nextX < WIDTH &&
+							nextY >= 0 && nextY < HEIGTH &&
+							grid[nextY][nextX] == BLANK) {
+							stop = false;
+							grid[nextY][nextX] = distance + 1;
+						}
+					}
+				}
+			}
+		}
+		distance++;
+	} while (!stop && grid[ey][ex] == BLANK);
+
+	//Если вышли по флагу то конечная точка не достижима
+	if (grid[ey][ex] == BLANK) return 0;
+
+	//(Этап поиска пути)
+	len = grid[ey][ex];
+	//Конечная точка - текущая
+	x = ex;
+	y = ey;
+	//Восстанавливать будем пока значение волны больше 0
+	while (distance > 0)
+	{
+		pointX[distance] = x;
+		pointY[distance] = y;
+		distance--;
+		for (vector = 0; vector < 4; ++vector) {
+			int nextX = x + dx[vector];
+			int nextY = y + dy[vector];
+			if (nextX >= 0 && nextX < WIDTH &&
+				nextY >= 0 && nextY < HEIGTH &&
+				grid[nextY][nextX] == distance) {
+				y = nextY;
+				x = nextX;
+			}
+		}
+	}
+	return len;
+}
+
+#pragma endregion
+
+#pragma region Полустепень захода
+/// <summary>
+/// Подсчет полу степеней захода графа с использованием матрицы смежностей
+/// </summary>
+/// <param name="matrix">Матрица смежностей</param>
+/// <param name="ref">Массив со степенями</param>
+/// <param name="size">Размер матрицы смежностей</param>
+void step(int** matrix, int* ref, int size) {
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if (matrix[i][j] == 1) ref[i] += 1;
+		}
+	}
+	//bucketSort(ref, n);
+}
+
+/// <summary>
+/// Подсчет степеней вершин смежных со стартовой точкой
+/// </summary>
+/// <param name="matrix">Матрица смежностей</param>
+/// <param name="lst">Список смежных вершин</param>
+/// <param name="start">стартовая точка</param>
+/// <param name="size">размер матрицы</param>
+void listAdj(int** matrix, List* lst,int start, int size) {
+	int step = 0; //кол-во ссылок на смежную вершину
+	for (int i = 0; i < size; i++)
+	{
+		if (matrix[start][i] == 1) {
+			for (int j = 0; j < size; j++)
+			{
+				if (matrix[i][j] == 1) step++;
+			}
+			insertList(lst, step, char(0x61 + i));
+			step = 0;
+		} 
+		
+	}
+	sortList(lst);
+}
+
+#pragma endregion
+
+int main()
+{
+	depthTravers(0);
+	init1dArrayNull(visited, n);
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	int** trevers = init2Array(n, n);
+	setLineValues(trevers, n, n, n * n,
+	  //a  b  c  d  e  f
+		1, 1, 1, 0, 0, 1, //a
+		0, 1, 0, 1, 1, 1, //b
+		0, 0, 1, 0, 0, 1, //c
+		1, 0, 0, 1, 0, 0, //d
+		0, 0, 0, 0, 1, 0, //e
+		0, 0, 0, 0, 1, 1);//f
+	print2dmatrix(trevers, n, n);
+	init1dArrayNull(refernce, n);
+	widthTravers(trevers, 0, n);
+	std::cout << std::endl;
+	print1dArray(refernce, n);
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	depthTraversStack(trevers, 0, n);
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	int* refer = new int[n];
+	init1dArrayNull(refer, n);
+	step(trevers, refer, n);
+	quickSort(refer,n);
+	print1dArray(refer, n);
+
+	List* lst = new List;
+	init(lst);
+	listAdj(trevers, lst,0,n);
+	printList(lst);
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	return 0;
+}
