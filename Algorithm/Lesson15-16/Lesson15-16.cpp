95667add6b3818fc18bdd1dc90832b77da94b3b1
Eduard <49712299+EdKrupenin@users.noreply.github.com>
2021-06-08 23:14:33 +0300

    Home Work

--- Algorithm/Lesson15-16/Lesson15-16.cpp
+++ Algorithm/Lesson15-16/Lesson15-16.cpp
@@ -1,7 +1,9 @@
 ﻿#include <iostream>
 #include "Resource/Lesson9-10.h"
+#include <string>
 typedef int K;
 typedef int htIndex;
+const int countWord = 26; //Константа - кол-во букв англ. алфавита
 
 int htSize; // глобальный размер хэш таблицы
 
@@ -9,22 +11,47 @@ int htSize; // глобальный размер хэш таблицы
 /// Расчет индекса в хэш таблице (массива)
 /// </summary>
 /// <param name="data">Ключ</param>
-/// <returns>Искомый индес</returns>
+/// <returns>Искомый индекс</returns>
 htIndex hash(T data) {
 	return data % htSize;
 }
 
 /// <summary>
+///  Простейшая хеш-функция.
+/// </summary>
+/// <param name="data">Строка</param>
+/// <param name="Type">Просто сложение или с участием степеней</param>
+/// <returns>сумма кодов символов</returns>
+htIndex hashString(std::string data, int Type) {
+	int length = data.length() - 1;
+	int hashCount = 0;
+	switch (Type)
+	{
+	case 0:
+		for (size_t i = 0; i <= length; i++)
+			hashCount += (int)data[length - i] * pow(countWord, i);
+		break;
+	case 1:
+		for (size_t i = 0; i <= length; i++)
+			hashCount += (int)data[i];
+		break;
+	default:
+		break;
+	}
+	return hashCount;
+}
+
+/// <summary>
 /// Добавление элементов в хэш-таблицу
 /// </summary>
-/// <param name="table">Ссылка на массив сос списками</param>
+/// <param name="table">Ссылка на массив со списками</param>
 /// <param name="data">ключ</param>
 /// <returns>Результат операции</returns>
 boolean insertNode(Knot** table, K data) {
-	Knot* p;  //Указтель на новый узел в который будт записан переданный ключ
-	Knot* p0; //Указатель на старую голову спискка к кторой будет добавлен новй узел
+	Knot* p;  //Указатель на новый узел в который будет записан переданный ключ
+	Knot* p0; //Указатель на старую голову списка к которой будет добавлен новый узел
 
-	htIndex bucket = hash(data); //Индекс хэш таблицы
+	htIndex bucket = hash(data); //Индекс хэш-таблицы
 	p = new Knot;
 	if (p == NULL) {
 		std::cout << "Out of memory\n";
@@ -49,39 +76,54 @@ Knot* findKnot(Knot** table, K data) {
 	//В цикле while будем до тех пор пока не найдем дату или не пройдем весь список
 	while (p && p->date != data)
 		p = p->next;
-	//если нашли дату то вернем узел, иначе вернем нул
+	//если нашли дату то вернем узел, иначе вернем null
 	return p;
 }
 
+/// <summary>
+/// Функция удаления ключа
+/// </summary>
+/// <param name="table">Ссылка на хэш-таблицу со списками</param>
+/// <param name="data">Ключ который надо удалить</param>
 void deleteKnot(Knot** table, K data) {
-	Knot* parent;
-	Knot* current;
-	parent = NULL;
-	htIndex bucket = hash(data);
-	current = table[bucket];
+	Knot* parent; //Предок
+	Knot* current; //Значение которое анализируем
+	parent = NULL; //Обнуляем предка
+	htIndex bucket = hash(data); //Вычисляем в каком именно списке предположительно
+	// будет наш ключ, с помощью хэш функции
+	current = table[bucket]; //достаем этот список из массива
+	//Проходим по списку пока он не кончиться или же пока не найдем ключ
 	while (current && current->date != data) {
 		parent = current;
 		current = current->next;
 	}
+	//Если список прошли до конца но ключа не нашли...
 	if (!current) {
 		std::cout << "Value not found\n";
 		return;
 	}
-	if (parent)
+	if (parent) //если ключ не в голове списка,
+		//то выкидываем current узел из списка убирая ссылку
 		parent->next = current->next;
-	else {
+	else { //Если ключ в голове списка переназначаем голову
 		table[bucket] = current->next;
 	}
+	//Удаляем узел
 	delete current;
 }
 
-
+/// <summary>
+/// Печать таблицы хэш функции
+/// </summary>
+/// <param name="table">Ссылка на массив</param>
+/// <param name="size">Размер таблицы</param>
 void printTable(Knot** table, int size) {
 	Knot* p;
+	//Перебираем весь массив со списками
 	for (int i = 0; i < size; i++)
 	{
-		p = table[i];
-		while (p)
+		p = table[i]; //берем подряд каждый список
+		while (p) //Пока не дойдем до конца списка
 		{
 			std::cout << p->date << " ";
 			p = p->next;
@@ -90,9 +132,41 @@ void printTable(Knot** table, int size) {
 	}
 	std::cout << std::endl;
 }
+
+/// <summary>
+/// Жадный алгоритм подсчета минимального количества монет
+/// </summary>
+/// <param name="denomination">Массив с номиналами монет</param>
+/// <param name="countCoin">Размер массива</param>
+/// <param name="sum">Сумма которую надо набрать</param>
+/// <returns>Количество монет</returns>
+int minimalCoin(int* denomination, int countCoin, int sum) {
+	int count = 0; //Счетчик монет
+	int countSum = 0; //Сумма из монет
+	//Проходим по всему массиву
+	for (size_t i = 0; i < countCoin; i++)
+	{
+		// Пока можем добавляем номинал, как только ушли за пределы суммы
+		// переходим к следующему номиналу
+		while ((countSum+denomination[i]) <= sum)
+		{
+			countSum += denomination[i];
+			count++;
+		} 
+		//Если набрали то что надо выходим
+		if (countSum == sum) return count;
+	}
+	return -1; //Из полученных монет нельзя набрать нужную сумму...
+}
+
 int main()
 {
 	htSize = 8;
 	int* arr;
-	std::cout << "Hello World!\n";
+
+	std::string words = "abc";
+	std::cout << "hash String = " << hashString(words,1) << std::endl;
+	const int MAX = 98; //Максимальное значение
+	int denomination[] = { 50, 10, 5, 2, 1 }; //номинал монет
+	std::cout << "minimal Coin = " << minimalCoin(denomination, 5, MAX) << std::endl;
 }
