df1f7d6354231ee6b736bdebae74765cdde0a16a
Eduard <ed.krupenin@gmail.com>
2021-11-22 14:28:22 +0300

    HWno1
    Первая домашняя работа

--- CppHardMoment/Lesson1/main.cpp
+++ CppHardMoment/Lesson1/main.cpp
@@ -1,51 +1,37 @@
 #include "LessonOne.h"
 
-ostream& operator<< (std::ostream &out, const Persons &person){
-    out<<person.surname<<" "<<person.name<<" "<<person.patronymic.value_or("") << " ";
-    return out;
-}
-
-bool operator==(const Persons &left, const Persons &right) {
-    return (tie(left.surname,left.name,left.patronymic) == tie(right.surname,right.name,right.patronymic));
-}
-
-bool operator<(const Persons &left, const Persons &right) {
-    return (tie(left.surname,left.name,left.patronymic) < tie(right.surname,right.name,right.patronymic));
-}
-
-ostream &operator<<(ostream &out, const PhoneNumber &phone) {
-    out<<"+"<<phone.countryCode<<"("<<phone.cityCode<<")"<<phone.number<<
-    phone.additionalNumber.value_or("");
-    return out;
-}
-
-bool operator==(const PhoneNumber &left, const PhoneNumber &right) {
-    return (tie(left.countryCode,left.cityCode,left.number, left.additionalNumber) == tie(right.countryCode,right.cityCode,right.number,right.additionalNumber));;
-}
-
-bool operator<(const PhoneNumber &left, const PhoneNumber &right) {
-    return (tie(left.countryCode,left.cityCode,left.number, left.additionalNumber) < tie(right.countryCode,right.cityCode,right.number,right.additionalNumber));;
-}
-
-void PhoneBook::SortByName() {
-    sort(this->container.begin(), this->container.end(),
-        [](pair<Persons, PhoneNumber>& lhs, pair<Persons, PhoneNumber>& rhs)
-        {
-            return tie(lhs.first.surname, lhs.first.name, lhs.first.patronymic) >
-                tie(rhs.first.surname, rhs.first.name, rhs.first.patronymic);
-        });
-}
-
-void PhoneBook::SortByPhone()
-{
-    sort(this->container.begin(), this->container.end(),
-        [](pair<Persons, PhoneNumber>& lhs, pair<Persons, PhoneNumber>& rhs)
-        {
-            return tie(lhs.second.countryCode, lhs.second.cityCode, lhs.second.number, lhs.second.additionalNumber) >
-                tie(rhs.second.countryCode, rhs.second.cityCode, rhs.second.number, rhs.second.additionalNumber);
-        });
-}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    ifstream file("PhoneBook.txt"); // путь к файлу PhoneBook.txt
+    PhoneBook book(file);
+    cout << book;
+
+    cout << "------SortByPhone-------" << endl;
+    book.SortByPhone();
+    cout << book;
+
+    cout << "------SortByName--------" << endl;
+    book.SortByName();
+    cout << book;
+
+    cout << "-----GetPhoneNumber-----" << endl;
+    // лямбда функция, которая принимает фамилию и выводит номер телефона этого    	человека, либо строку с ошибкой
+    auto print_phone_number = [&book](const string& surname) {
+        cout << surname << "\t";
+        auto answer = book.GetPhoneNumber(surname);
+        if (get<0>(answer).empty())
+            cout << get<1>(answer);
+        else
+            cout << get<0>(answer);
+        cout << endl;
+    };
+
+    // вызовы лямбды
+    print_phone_number("Ivanov");
+    print_phone_number("Petrov");
+
+    cout << "----ChangePhoneNumber----" << endl;
+    book.ChangePhoneNumber(Persons{ "Kotov", "Vasilii", "Eliseevich" }, PhoneNumber{ 7, 123, "15344458", nullopt });
+    book.ChangePhoneNumber(Persons{ "Mironova", "Margarita", "Vladimirovna" }, PhoneNumber{ 16, 465, "9155448", 13 });
+    cout << book;
+
 }
