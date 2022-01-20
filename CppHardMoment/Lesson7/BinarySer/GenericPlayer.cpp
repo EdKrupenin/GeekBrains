6a04aacee2b8450583dc5a88aa12d88bf923d0a6
Eduard <ed.krupenin@gmail.com>
2022-01-20 11:07:17 +0300

    main into
    Сборка в Main

---
+++ CppHardMoment/Lesson7/BinarySer/GenericPlayer.cpp
@@ -0,0 +1,46 @@
+#include "GenericPlayer.h"
+
+GenericPlayer::GenericPlayer(std::string name) : Hand(), m_Name(name)
+{
+
+}
+
+bool GenericPlayer::IsBoosted() const
+{
+	if (h_get_total() > 21)
+	{
+		return true;
+	}
+	return false;
+}
+
+void GenericPlayer::Bust() const
+{
+	if (IsBoosted())
+		std::cout << m_Name << " has Boosted!!!" << std::endl;
+}
+
+const std::string GenericPlayer::get_Name() const
+{
+	return m_Name;
+}
+
+std::ostream& operator<<(std::ostream& out, const GenericPlayer& player)
+{
+	out << player.get_Name() << ":\t";
+	if (!player.m_Cards.empty()) {
+		for (auto it = player.m_Cards.begin(); it != player.m_Cards.end(); ++it) {
+			out << *(*it) << "\t";
+		}
+
+		if (player.h_get_total() != 0) {
+			out << "(" << player.h_get_total() << ")" << std::endl;
+		}
+	}
+	else
+	{
+		out << "Empty";
+	}
+
+	return out;
+}
---
+++ CppHardMoment/Lesson7/BinarySer/Player.cpp
@@ -0,0 +1,41 @@
+#include "Player.h"
+
+Player::Player(const std::string name) : GenericPlayer(name)
+{
+}
+
+Player::~Player()
+{
+}
+
+bool Player::IsHitting() const
+{
+	std::cout << get_Name() << ", do you want a hit? (Y/N): ";
+	char response;
+	std::cin >> response;
+	while (std::cin.fail() || (std::cin.peek() != '\n') || (response != 'Y' && response != 'N')) {
+		std::cout << "IDIOT !! Enter again !\n";
+		std::cin.clear();
+		std::cin.ignore();
+		std::cin.sync();
+		std::cout << "Enter Y or N : ";
+		std::cin >> response;
+	}
+	std::cout << std::endl;
+	return (response == 'Y');
+}
+
+void Player::Win() const
+{
+	std::cout << "Player " << get_Name() << " is Win";
+}
+
+void Player::Lose() const
+{
+	std::cout << "Player " << get_Name() << " is Lose";
+}
+
+void Player::Push() const
+{
+	std::cout << "Player " << get_Name() << " is Push";
+}
